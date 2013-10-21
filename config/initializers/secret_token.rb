09dcd4657915afb990dffbaed314c4adc7f17773
Mark Wetzel <mark@markwetzel.com>
2013-10-21 12:15:44 -0400

    Initial.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Biketurnout::Application.config.secret_key_base = 'e4696d74b050afe2b3eb98b3cd793ccf70d4ad86f4dbb514c118aa5e52312b9e955a5162e2d70542a7e1b1ac7f88e21c50546e1576f9a63da9690c93ff43ab4e'
